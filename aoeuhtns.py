a19ccce8ae5ff28d99baaec7e710af9eaca9bc90
ArminIrannejad <armin.irannejad@gmail.com>
2024-04-06 12:53:01 +0200

    removed JSON from raw data

--- aoeuhtns.py
+++ aoeuhtns.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 import requests
 
-response = requests.get("https://tipsxtra.se/statistics/exportcsv/details/1")
+response = requests.get("https://tipsxtra.se/statistics/exportcsv/details/1").json()['csv']
 
-with open('raw_data.csv', 'wb') as file:
-    file.write(response.content)
+with open('raw_data.csv', 'w') as file:
+    file.write(response)
 
+df = pd.read_csv('raw_data.csv', delimiter=';')
+print(df.head())
 
