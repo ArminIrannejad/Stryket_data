1f2cc47cd58170d01294647815dd6bf95978cbdd
ArminIrannejad <armin.irannejad@gmail.com>
2024-04-06 13:13:25 +0200

    refactor

--- aoeuhtns.py
+++ aoeuhtns.py
@@ -2,11 +2,16 @@ import pandas as pd
 import numpy as np
 import requests
 
-response = requests.get("https://tipsxtra.se/statistics/exportcsv/details/1").json()['csv']
+def get_data():
+    response = requests.get("https://tipsxtra.se/statistics/exportcsv/details/1").json()['csv']
+    with open('raw_data.csv', 'w') as file:
+        file.write(response)
 
-with open('raw_data.csv', 'w') as file:
-    file.write(response)
+    
+def main():
+    get_data()
+    df = pd.read_csv('raw_data.csv', delimiter=';')
+    print(df.head())
 
-df = pd.read_csv('raw_data.csv', delimiter=';')
-print(df.head())
-
+if __name__ == "__main__":
+    main()
